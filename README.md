f8f98887320538471f6645069d1e4f6d660e6d69
WalkerTian <903895182@qq.com>
2024-06-26 04:18:38 +0800

    \suoxi/

--- README.md
+++ README.md
@@ -1,15 +1,16 @@
 # FastBotSpawn
 -----
 
-一个[MCDReforged](https://github.com/Fallen-Breath/MCDReforged)插件，可以批量召唤假人并设置前缀，最好与fabric-carpet的配置项fakePlayerNamePrefix保持一致。
+一个[MCDReforged](https://github.com/Fallen-Breath/MCDReforged)插件，可以批量召唤假人并设置前缀，最好与fabric-carpet的配置项`fakePlayerNamePrefix`保持一致。
 
 依赖于fabric-carpet
 
 ### 指令
 ```
-!!b set bot_ 设置前缀，bot_可替换，推荐和Carpet配置同步，没有就留空
+!!b pre bot_ 设置前缀，bot_可替换，推荐和Carpet配置同步，没有就留空
+!!b suf _fake 设置后缀，_fake可替换，推荐和Carpet配置同步，没有就留空
 !!b limit [number] 设置最大生成数量，默认为10
-!!b clear 清除设置的前缀
+!!b clear 清除设置的前缀&后缀
 !!b spawn [mini] [max] 批量召唤假人
 !!b drop [mini] [max] 批量控制丢出全部物品
 !!b kill [mini] [max] 批量下线假人
--- fbs_plugin/__init__.py
+++ fbs_plugin/__init__.py
@@ -4,29 +4,37 @@ import json
 
 PLUGIN_METADATA = {
     'id': 'bot',
-    'version': '1.1.2',
+    'version': '1.1.3',
     'name': 'FastBotSpawn',
     'description': 'A plugin with multiple functions',
     'author': 'WalkerTian',
     'link': 'https://github.com/Walkersifolia/FastBotSpawn'
 }
 
+suffix = ''
 prefix = ''
 limit = 10
 config_file = os.path.join('config', 'FastBotSpawn.json')
 
 def save_config(server: ServerInterface):
     with open(config_file, 'w') as f:
-        json.dump({'prefix': prefix, 'limit': limit}, f)
+        json.dump({'prefix': prefix, 'limit': limit, 'suffix': suffix}, f)
 
 def load_config(server: ServerInterface):
-    global prefix, limit
+    global prefix, limit ,suffix
     if os.path.exists(config_file):
         with open(config_file, 'r') as f:
             data = json.load(f)
             prefix = data.get('prefix', '')
+            suffix = data.get('suffix', '')
             limit = data.get('limit', 10)
 
+def clear_suffix(server: ServerInterface):
+    global suffix
+    suffix = ''
+    save_config(server)
+    server.logger.info('Suffix cleared')
+    
 def clear_prefix(server: ServerInterface):
     global prefix
     prefix = ''
@@ -37,38 +45,44 @@ def clear_prefix(server: ServerInterface):
 def spawn_bots(source: CommandSource, start: int, end: int):
     player = source.player
     for i in range(start, end+1):
-        source.get_server().execute(f'/execute at {player} run player {prefix}{i} spawn in survival')
+        source.get_server().execute(f'/execute at {player} run player {prefix}{i}{suffix} spawn in survival')
 
 @new_thread(PLUGIN_METADATA['name'])
 def drop_items(source: CommandSource, start: int, end: int):
     for i in range(start, end+1):
-        source.get_server().execute(f'/player {prefix}{i} dropStack all')
+        source.get_server().execute(f'/player {prefix}{i}{suffix} dropStack all')
 
 @new_thread(PLUGIN_METADATA['name'])
 def kill_bots(source: CommandSource, start: int, end: int):
     for i in range(start, end+1):
-        source.get_server().execute(f'/player {prefix}{i} kill')
+        source.get_server().execute(f'/player {prefix}{i}{suffix} kill')
 
 def show_help_message(server: ServerInterface, info: Info):
     server.reply(info, '\n----------欢迎使用FastBotSpawn插件----------\n')
-    server.reply(info, '§6!!b set bot_§r 设置前缀，§6bot_§r可替换，推荐和Carpet配置同步，没有就留空')
+    server.reply(info, '§6!!b pre bot_§r 设置前缀，§6bot_§r可替换，推荐和Carpet配置同步，没有就留空')
+    server.reply(info, '§6!!b suf _fake§r 设置后缀，§6_fake§r可替换，推荐和Carpet配置同步，没有就留空')
     server.reply(info, '§6!!b limit [number]§r 设置最大生成数量，默认为10')
-    server.reply(info, '§6!!b clear§r 清除设置的前缀')
+    server.reply(info, '§6!!b clear§r 清除设置的前缀&后缀')
     server.reply(info, '§6!!b spawn [mini] [max]§r 批量召唤假人')
     server.reply(info, '§6!!b drop [mini] [max]§r 批量控制假人丢出全部物品')
     server.reply(info, '§6!!b kill [mini] [max]§r 批量下线假人')
     server.reply(info, '§6[mini]§r和§6[max]§r是最小和最大的序号，差值不能超过10，不写默认为1-10')
 
 def on_user_info(server: ServerInterface, info: Info):
-    global prefix, limit
+    global prefix, limit ,suffix
     if info.is_player:
-        if info.content.startswith('!!b set'):
+        if info.content.startswith('!!b pre'):
             prefix = info.content.split(' ')[2]
             save_config(server)
             server.reply(info, f'假人前缀已设置为 "{prefix}"')
+        elif info.content.startswith('!!b suf'):
+            suffix = info.content.split(' ')[2]
+            save_config(server)
+            server.reply(info, f'假人后缀已设置为 "{suffix}"')
         elif info.content == '!!b clear':
             clear_prefix(server)
-            server.reply(info, '假人前缀已清除')
+            clear_suffix(server)
+            server.reply(info, '假人前缀&后缀已清除')
         elif info.content.startswith('!!b limit'):
             limit = info.content.split(' ')[2]
             save_config(server)
